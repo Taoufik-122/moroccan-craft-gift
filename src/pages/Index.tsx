55311fbb90e1df6c325ce8679f72580010ab5719
Taoufik-122 <toufikamb@gmail.com>
2025-12-22 23:59:00 +0100

    Add Meta Pixel and SPA pageview tracking V1

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -35,24 +35,8 @@ const Index = () => {
           <meta property="og:image" content="https://moroccancraftgift.com/logo.png" />
           <meta property="og:url" content="https://moroccancraftgift.com/" />
           <link rel="canonical" href="https://moroccancraftgift.com/" />
-{/* Meta Pixel Code */}
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1693587907889582');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1693587907889582&ev=PageView&noscript=1"
-/></noscript>
-{/* End Meta Pixel Code -->
-          {/* Structured Data JSON-LD */}
+
+     {/*     {/* Structured Data JSON-LD */}
           <script type="application/ld+json">
             {`
             {
